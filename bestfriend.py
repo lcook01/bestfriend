e4224727fb0386d1f8ab9ed9c9894662d2dc9076
Lillyann Cook <lillyanncook46@gmail.com>
2022-12-06 14:41:00 -0500

    State of the project on 12/6/22

--- bestfriend.py
+++ bestfriend.py
@@ -1,2 +1,11 @@
-first_name = input("What is your name?")
-print(f"Hello {first_name}")
+print("=========================")
+print("   BEST FRIEND MAKER")
+print("=========================")
+
+print("Hi. I'm the computer.")
+first_name = (f"What's your name?")
+fcolor = print(f"Hi, {first_name}! What's your favorite color?")
+print(f"No Way! My favorite color is also {fcolor}!")
+fmovie = (f"What's your favorite movie?")
+print(f" What!/! My favorite movie is also {fmovie}!")
+fgame = (f" What is your favorite game?")
